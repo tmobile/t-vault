861915300e0f66a74147888c0fef77b81e7bac97
Sivakumar Nagarajan <sivanag1974@gmail.com>
2018-12-06 14:33:45 -0800

    Upgraded vault to 1.0.0 version

--- tvaultapi/src/main/java/com/tmobile/cso/vault/api/service/SysService.java
+++ tvaultapi/src/main/java/com/tmobile/cso/vault/api/service/SysService.java
@@ -101,19 +101,25 @@ public class  SysService {
 			      put(LogMessage.APIURL, ThreadLocalContext.getCurrentMap().get(LogMessage.APIURL).toString()).
 			      build()));
 		Response response = reqProcessor.process("/seal-status","{}","");
-		
 		if(HttpStatus.OK.equals(response.getHttpstatus())) {
 			return ResponseEntity.status(HttpStatus.OK).body("{\"messages\":"+response.getResponse()+"}");
 		} else {
-			log.error(JSONUtil.getJSON(ImmutableMap.<String, String>builder().
-				      put(LogMessage.USER, ThreadLocalContext.getCurrentMap().get(LogMessage.USER).toString()).
-					  put(LogMessage.ACTION, "Check Seal-status").
-				      put(LogMessage.MESSAGE, "Getting Vault Seal-Status failed.").
-				      put(LogMessage.RESPONSE, response.getResponse()).
-				      put(LogMessage.STATUS, response.getHttpstatus().toString()).
-				      put(LogMessage.APIURL, ThreadLocalContext.getCurrentMap().get(LogMessage.APIURL).toString()).
-				      build()));
-			return ResponseEntity.status(response.getHttpstatus()).body("{\"Error\":[\"Unable to get Seal-Status information\"]}");
+			// If no response for http, then try https
+			response = reqProcessor.process("/v2/seal-status","{}","");
+			if(HttpStatus.OK.equals(response.getHttpstatus())) {
+				return ResponseEntity.status(HttpStatus.OK).body("{\"messages\":"+response.getResponse()+"}");
+			}
+			else {
+				log.error(JSONUtil.getJSON(ImmutableMap.<String, String>builder().
+					      put(LogMessage.USER, ThreadLocalContext.getCurrentMap().get(LogMessage.USER).toString()).
+						  put(LogMessage.ACTION, "Check Seal-status").
+					      put(LogMessage.MESSAGE, "Getting Vault Seal-Status failed.").
+					      put(LogMessage.RESPONSE, response.getResponse()).
+					      put(LogMessage.STATUS, response.getHttpstatus().toString()).
+					      put(LogMessage.APIURL, ThreadLocalContext.getCurrentMap().get(LogMessage.APIURL).toString()).
+					      build()));
+				return ResponseEntity.status(response.getHttpstatus()).body("{\"Error\":[\"Unable to get Seal-Status information\"]}");
+			}
 		}
 	}
 	/**
