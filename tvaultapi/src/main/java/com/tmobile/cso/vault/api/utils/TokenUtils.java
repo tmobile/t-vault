fb8e23efba859c629f5d6323fde98f376814d102
Sarath Mohan <sarathm4@gmail.com>
2018-12-13 13:32:28 +0530

    Clean up

--- tvaultapi/src/main/java/com/tmobile/cso/vault/api/utils/TokenUtils.java
+++ tvaultapi/src/main/java/com/tmobile/cso/vault/api/utils/TokenUtils.java
@@ -40,27 +40,6 @@ public class TokenUtils {
 
 	}
 
-	public String generatePowerToken(String token) {
-		String roleName = "vault-power-user-role"; 
-		Response appRoleRes = ControllerUtil.reqProcessor.process("/auth/approle/role/readRoleID","{\"role_name\":\""+roleName+"\"}",token); //TODO: How to get this role
-		String role_id = null;
-		if (appRoleRes.getResponse() != null && ((LinkedHashMap<String, String>)ControllerUtil.parseJson(appRoleRes.getResponse()).get("data")) != null) {
-			role_id = ((LinkedHashMap<String, String>)ControllerUtil.parseJson(appRoleRes.getResponse()).get("data")).get("role_id");
-		}
-		
-		Response secidRes = ControllerUtil.reqProcessor.process("/auth/approle/secretid/lookup","{\"role_name\":\""+roleName+"\"}",token);
-		String secret_id = ((LinkedHashMap<String, String>)ControllerUtil.parseJson(secidRes.getResponse()).get("data")).get("secret_id");
-		String powerToken = null;
-		if (role_id != null && secret_id != null) {
-			AppRoleIdSecretId appRoleIdSecretId = new AppRoleIdSecretId(role_id, secret_id);
-			
-			Response loginRes = ControllerUtil.reqProcessor.process("/auth/approle/login",JSONUtil.getJSON(appRoleIdSecretId),"");
-			powerToken = ((LinkedHashMap<String, String>)ControllerUtil.parseJson(loginRes.getResponse()).get("auth")).get("client_token");
-		}
-		return powerToken;
-		
-	}
-	
 	public String getSelfServiceToken() {
 		log.debug(JSONUtil.getJSON(ImmutableMap.<String, String>builder().
 				put(LogMessage.USER, ThreadLocalContext.getCurrentMap().get(LogMessage.USER).toString()).
@@ -109,9 +88,22 @@ public class TokenUtils {
 	 * @param token
 	 */
 	public void revokePowerToken(String token) {
-		//TODO: Error handling, etc
-		ControllerUtil.reqProcessor.process("/auth/tvault/revoke","{}", token);
+		Response response = ControllerUtil.reqProcessor.process("/auth/tvault/revoke","{}", token);
+		if (HttpStatus.NO_CONTENT.equals(response.getHttpstatus()) || HttpStatus.OK.equals(response.getHttpstatus())) {
+			log.debug(JSONUtil.getJSON(ImmutableMap.<String, String>builder().
+					put(LogMessage.USER, ThreadLocalContext.getCurrentMap().get(LogMessage.USER).toString()).
+					put(LogMessage.ACTION, "revokePowerToken").
+					put(LogMessage.MESSAGE, String.format ("SelfService token successfully revoked")).
+					put(LogMessage.APIURL, ThreadLocalContext.getCurrentMap().get(LogMessage.APIURL).toString()).
+					build()));
+		}
+		else {
+			log.debug(JSONUtil.getJSON(ImmutableMap.<String, String>builder().
+					put(LogMessage.USER, ThreadLocalContext.getCurrentMap().get(LogMessage.USER).toString()).
+					put(LogMessage.ACTION, "revokePowerToken").
+					put(LogMessage.MESSAGE, String.format ("SelfService token revoke failed")).
+					put(LogMessage.APIURL, ThreadLocalContext.getCurrentMap().get(LogMessage.APIURL).toString()).
+					build()));
+		}
 	}
-
-
 }
