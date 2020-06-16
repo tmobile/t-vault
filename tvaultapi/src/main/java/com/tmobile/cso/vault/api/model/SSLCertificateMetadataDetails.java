53b34941582e7e304250cc000dfb860aaf1bbb41
Udaya Bhaskararaokothuru(UST <UdayaBhaskaraRao.Kothuru1@T-Mobile.com>
2020-06-16 19:29:21 -0400

    COCICCS-2273,COCICCS-2274 provided fix

--- tvaultapi/src/main/java/com/tmobile/cso/vault/api/model/SSLCertificateMetadataDetails.java
+++ tvaultapi/src/main/java/com/tmobile/cso/vault/api/model/SSLCertificateMetadataDetails.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class SSLCertificateMetadataDetails implements Serializable {
 
     private String name;
-    private String id;
+    private int id;
     private String description;
     private String ownerId;
     private String ownerEmail;
@@ -19,11 +19,11 @@ public class SSLCertificateMetadataDetails implements Serializable {
         this.name = name;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
 
