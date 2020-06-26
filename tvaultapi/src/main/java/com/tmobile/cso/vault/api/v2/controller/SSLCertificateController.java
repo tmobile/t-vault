af9866e4c0feb643bb4a229f59cbe7af526788ce
nlokana1 <nithya.lokanathan1@t-mobile.com>
2020-06-27 01:34:40 +0530

    List certificate API and UI updates

--- tvaultapi/src/main/java/com/tmobile/cso/vault/api/v2/controller/SSLCertificateController.java
+++ tvaultapi/src/main/java/com/tmobile/cso/vault/api/v2/controller/SSLCertificateController.java
@@ -69,9 +69,9 @@ public class SSLCertificateController {
 	 */
 	@ApiOperation(value = "${SSLCertificateController.getssl.value}", notes = "${SSLCertificateController.getssl.notes}")
 	@GetMapping(value="/v2/sslcert", produces="application/json")
-	public ResponseEntity<String> getCertificates(HttpServletRequest request, @RequestHeader(value="vault-token") String token, @RequestParam(name="freeText", required = false) String freeText)throws Exception{
+	public ResponseEntity<String> getCertificates(HttpServletRequest request, @RequestHeader(value="vault-token") String token, @RequestParam(name="certificateName", required = false) String certName)throws Exception{
 		UserDetails userDetails = (UserDetails) ((HttpServletRequest) request).getAttribute("UserDetails");
-		return sslCertificateService.getServiceCertificates(token, userDetails, freeText);
+		return sslCertificateService.getServiceCertificates(token, userDetails, certName);
 	}
 
 }
