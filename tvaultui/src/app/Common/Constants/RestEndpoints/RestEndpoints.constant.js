60bb7ab7c182c09efc1ec8f89f04170644316ea9
alexanantha <63574989+alexanantha@users.noreply.github.com>
2020-07-13 18:40:16 +0530

    UI changes for add approle to certificate

--- tvaultui/src/app/Common/Constants/RestEndpoints/RestEndpoints.constant.js
+++ tvaultui/src/app/Common/Constants/RestEndpoints/RestEndpoints.constant.js
@@ -411,6 +411,10 @@ readTextFile("../apiUrls.json");
             url: '/v2/sslcert/certificates',
             method: 'GET'
         }, {
+            name: 'addApproleToCertificate',
+            url: '/v2/sslcert/approle',
+            method: 'POST'
+        }, {
             name: 'downloadCertificate',
             url: '/v2/sslcert/certificates/download',
             method: 'POST'
