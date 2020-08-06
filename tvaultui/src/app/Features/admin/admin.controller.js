cbb9861e4f826fb6b1571c51c29db52fb271a8cb
Udaya Bhaskararaokothuru(UST <UdayaBhaskaraRao.Kothuru1@T-Mobile.com>
2020-08-06 17:39:54 -0400

    UI fixes

--- tvaultui/src/app/Features/admin/admin.controller.js
+++ tvaultui/src/app/Features/admin/admin.controller.js
@@ -724,7 +724,7 @@
                     $scope.showInputLoader.show = false;
                     $scope.errorMessage = UtilityService.getAParticularErrorMessage('ERROR_GENERAL');
                     $scope.error('md');
-                }
+                }                
                 $scope.isTargetSystemListLoading = false;
             },
             function (error) {
@@ -1480,6 +1480,7 @@
             $scope.targetSystemServiceType = { "type": "new" };
             $scope.getTargetSystems();
             $scope.multiSanDnsName.name='';
+            $scope.certDnsErrorMessage='';
         }
 
         $scope.replaceSpacesCertName = function () {
@@ -1764,8 +1765,12 @@
                 $scope.isCertCollapsed = true;
                 if ($scope.multiSanDnsName.name !== null && $scope.multiSanDnsName.name !== undefined 
                     && $scope.multiSanDnsName.name != "") {
-                        $scope.multiSanDnsName.name ="";
+                        $scope.multiSanDnsName.name ="";       
+                           if($scope.certDnsErrorMessage!== null)    {
+                            $scope.certDnsErrorMessage = "";
+                        }
                 }
+        
     
             } else if (index == 2 ) {
                 $scope.isTargetCollapsed = true;
@@ -1777,6 +1782,9 @@
                 if ($scope.multiSanDnsName.name !== null && $scope.multiSanDnsName.name !== undefined 
                     && $scope.multiSanDnsName.name != "") {
                         $scope.multiSanDnsName.name ="";
+                        if($scope.certDnsErrorMessage!== null)    {
+                            $scope.certDnsErrorMessage = "";
+                        }
                 }
                 if ($scope.serviceListTableOptions.length >0) {
                     setTargetSystemServiceList("Select service", $scope.serviceListTableOptions);
