e7c3d3a6574ebceecd5bb22ec314878fad52630c
Sarath Mohan <sarath.mohan11@t-mobile.com>
2020-08-06 17:19:11 +0530

    Api to get group object id from Azure AD

--- tvaultapi/src/main/java/com/tmobile/cso/vault/api/common/TVaultConstants.java
+++ tvaultapi/src/main/java/com/tmobile/cso/vault/api/common/TVaultConstants.java
@@ -121,6 +121,10 @@ public class TVaultConstants {
         EMAIL_TEMPLATE_IMAGE_IDS.put("adduser", "templates/images/adduser.png");
     }
     public static final String AUTH_TYPE_OIDC = "oidc";
+    public static final String HTTP_CONTENT_TYPE_JSON = "application/json";
+    public static final String HTTP_CONTENT_TYPE_URL_ENCODED = "application/x-www-form-urlencoded";
+
+
 
     public static final String ALIAS_MOUNT_ACCESSOR = "accessor";
     public static final String ENTITY_NAME = "name";
