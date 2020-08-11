8e7b41570ca9c63f6bd5b985a619ccc108bad329
Nithin Nazeer <Nithin.Nazeer1@t-mobile.com>
2020-08-11 11:47:45 +0530

    group read permission changes

--- tvaultapi/src/main/java/com/tmobile/cso/vault/api/controller/OIDCUtil.java
+++ tvaultapi/src/main/java/com/tmobile/cso/vault/api/controller/OIDCUtil.java
@@ -25,19 +25,32 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.tmobile.cso.vault.api.common.TVaultConstants;
+import com.tmobile.cso.vault.api.exception.LogMessage;
+import com.tmobile.cso.vault.api.model.DirectoryObjects;
+import com.tmobile.cso.vault.api.model.DirectoryUser;
+import com.tmobile.cso.vault.api.model.GroupAliasRequest;
 import com.tmobile.cso.vault.api.model.OIDCEntityResponse;
+import com.tmobile.cso.vault.api.model.OIDCIdentityGroupRequest;
+import com.tmobile.cso.vault.api.model.OIDCLookupEntityRequest;
 import com.tmobile.cso.vault.api.process.RequestProcessor;
 import com.tmobile.cso.vault.api.process.Response;
+import com.tmobile.cso.vault.api.service.DirectoryService;
+import com.tmobile.cso.vault.api.utils.JSONUtil;
+import com.tmobile.cso.vault.api.utils.ThreadLocalContext;
 
 @Component
 public class OIDCUtil {
@@ -54,25 +67,20 @@ public class OIDCUtil {
 	private static RequestProcessor reqProcessor;
 	public static final Logger log = LogManager.getLogger(OIDCUtil.class);
 	
-	@Autowired(required = true)
-	public void setreqProcessor(RequestProcessor reqProcessor) {
-		OIDCUtil.reqProcessor = reqProcessor;
-	}
-
-	/**
-	 * Method to get requestProcessor
-	 * @return
-	 */
-	public static RequestProcessor getReqProcessor() {
-		return OIDCUtil.reqProcessor;
-	}
+	@Autowired
+	private RequestProcessor reqProcessor;
+	
+	@Autowired
+	private DirectoryService directoryService;
+	
+	public static final Logger log = LogManager.getLogger(OIDCUtil.class);
 	
 	/**
 	 * Fetch mount accessor id from oidc mount
 	 * @param response
 	 * @return
 	 */
-	public static String fetchMountAccessorForOidc(String token) {
+	public String fetchMountAccessorForOidc(String token) {
 		Response response = reqProcessor.process("/sys/list", "{}", token);
 		if (HttpStatus.OK.equals(response.getHttpstatus())) {
 			Map<String, String> metaDataParams = null;
@@ -97,7 +105,12 @@ public class OIDCUtil {
 		return null;
 	}
 	
-	public static OIDCEntityResponse getEntityLookUpResponse(String authMountResponse) {
+	/**
+	 * Get Entity LookUp Response
+	 * @param authMountResponse
+	 * @return
+	 */
+	public OIDCEntityResponse getEntityLookUpResponse(String authMountResponse) {
 		Map<String, String> metaDataParams = null;
 		JsonParser jsonParser = new JsonParser();
 		JsonObject object = ((JsonObject) jsonParser.parse(authMountResponse)).getAsJsonObject("data");
@@ -114,6 +127,51 @@ public class OIDCUtil {
 			}
 		}
 		return oidcEntityResponse;
+	}
+	
+	/**
+	 * Update Group Policies
+	 * @param token
+	 * @param policies
+	 * @param groupName
+	 * @return
+	 */
+	public Response updateGroupPolicies(String token, List<String> policies, String groupName) {
+
+		// Delete Group By Name
+		Response response = deleteGroupByName(token, groupName);
+		if (response.getHttpstatus().equals(HttpStatus.OK)) {
+			OIDCIdentityGroupRequest oidcIdentityGroupRequest = new OIDCIdentityGroupRequest();
+			oidcIdentityGroupRequest.setName(groupName);
+			oidcIdentityGroupRequest.setPolicies(policies);
+			oidcIdentityGroupRequest.setType(TVaultConstants.EXTERNAL_TYPE);
+			String canonicalID = updateIdentityGroupByName(token, oidcIdentityGroupRequest);
+			if (canonicalID != null) {
+				String mountAccessor = fetchMountAccessorForOidc(token);
+				// Object Id call object Api
+				String objectId = "";
+				GroupAliasRequest groupAliasRequest = new GroupAliasRequest();
+				groupAliasRequest.setCanonical_id(canonicalID);
+				groupAliasRequest.setMount_accessor(mountAccessor);
+				groupAliasRequest.setName(objectId);
+				return createGroupAlias(token, groupAliasRequest);
+			} else {
+				response.setHttpstatus(HttpStatus.BAD_REQUEST);
+				return response;
+			}
+		}
+		response.setHttpstatus(HttpStatus.BAD_REQUEST);
+		return response;
+	}
+	
+	/**
+	 * Delete Group By Name
+	 * @param token
+	 * @param name
+	 * @return
+	 */
+	public Response deleteGroupByName(String token, String name) {
+		return reqProcessor.process("/identity/group/name", "{\"name\":\"" + name + "\"}", token);
 	}
 
 	/**
@@ -281,5 +339,103 @@ public class OIDCUtil {
 					build()));
 		}
 		return null;
+	}
+     /*
+	 * Update Identity Group By Name
+	 * @param token
+	 * @param oidcIdentityGroupRequest
+	 * @return
+	 */
+	public String updateIdentityGroupByName(String token, OIDCIdentityGroupRequest oidcIdentityGroupRequest) {
+		String jsonStr = JSONUtil.getJSON(oidcIdentityGroupRequest);
+		Response response = reqProcessor.process("/identity/group/name/update", jsonStr, token);
+
+		if (HttpStatus.OK.equals(response.getHttpstatus())) {
+			Map<String, String> metaDataParams = null;
+			JsonParser jsonParser = new JsonParser();
+			JsonObject data = ((JsonObject) jsonParser.parse(response.getResponse())).getAsJsonObject("data");
+			if (data != null) {
+				JsonObject object = ((JsonObject) jsonParser.parse(response.getResponse())).getAsJsonObject("data");
+
+				metaDataParams = new Gson().fromJson(object.toString(), Map.class);
+
+				String canonicalId = "";
+				for (Map.Entry m : metaDataParams.entrySet()) {
+					if ("id".equals(m.getKey())) {
+						canonicalId = m.getValue().toString();
+						break;
+					}
+				}
+				return canonicalId;
+			}
+		}
+		return null;
+
+	}
+	
+	/**
+	 * Create Group Alias
+	 * @param token
+	 * @param groupAliasRequest
+	 * @return
+	 */
+	public Response createGroupAlias(String token, GroupAliasRequest groupAliasRequest) {
+		String jsonStr = JSONUtil.getJSON(groupAliasRequest);
+		return reqProcessor.process("/identity/group-alias", jsonStr, token);
+	}
+	
+	
+	/**
+	 * Common method to Fetch OIDC entity details
+	 * 
+	 * @param token
+	 * @param username
+	 * @return
+	 */
+	public ResponseEntity<OIDCEntityResponse> oidcFetchEntityDetails(String token, String username) {
+		String mountAccessor = fetchMountAccessorForOidc(token);
+		if (!StringUtils.isEmpty(mountAccessor)) {
+			ResponseEntity<DirectoryObjects> response = directoryService.searchByCorpId(username);
+			String aliasName = "";
+			Object[] results = response.getBody().getData().getValues();
+			for (Object tp : results) {
+				aliasName = ((DirectoryUser) tp).getUserEmail();
+			}
+			OIDCLookupEntityRequest oidcLookupEntityRequest = new OIDCLookupEntityRequest();
+			oidcLookupEntityRequest.setAlias_name(aliasName);
+			oidcLookupEntityRequest.setAlias_mount_accessor(mountAccessor);
+			return entityLookUp(token, oidcLookupEntityRequest);
+		}
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new OIDCEntityResponse());
+	}
+	
+	/**
+	 * Entity Lookup 
+	 * @param token
+	 * @param oidcLookupEntityRequest
+	 * @return
+	 */
+	public ResponseEntity<OIDCEntityResponse> entityLookUp(String token,
+			OIDCLookupEntityRequest oidcLookupEntityRequest) {
+		String jsonStr = JSONUtil.getJSON(oidcLookupEntityRequest);
+		OIDCEntityResponse oidcEntityResponse = new OIDCEntityResponse();
+		Response response = reqProcessor.process("/identity/lookup/entity", jsonStr, token);
+		if (response.getHttpstatus().equals(HttpStatus.OK)) {
+			oidcEntityResponse = getEntityLookUpResponse(response.getResponse());
+			log.debug(JSONUtil.getJSON(ImmutableMap.<String, String> builder()
+					.put(LogMessage.USER, ThreadLocalContext.getCurrentMap().get(LogMessage.USER))
+					.put(LogMessage.ACTION, "entityLookUp")
+					.put(LogMessage.MESSAGE, "Successfully received entity lookup")
+					.put(LogMessage.STATUS, response.getHttpstatus().toString())
+					.put(LogMessage.APIURL, ThreadLocalContext.getCurrentMap().get(LogMessage.APIURL)).build()));
+			return ResponseEntity.status(response.getHttpstatus()).body(oidcEntityResponse);
+		} else {
+			log.error(JSONUtil.getJSON(ImmutableMap.<String, String> builder()
+					.put(LogMessage.USER, ThreadLocalContext.getCurrentMap().get(LogMessage.USER))
+					.put(LogMessage.ACTION, "entityLookUp").put(LogMessage.MESSAGE, "Failed entity Lookup")
+					.put(LogMessage.STATUS, response.getHttpstatus().toString())
+					.put(LogMessage.APIURL, ThreadLocalContext.getCurrentMap().get(LogMessage.APIURL)).build()));
+			return ResponseEntity.status(response.getHttpstatus()).body(oidcEntityResponse);
+		}
 	}
 }
