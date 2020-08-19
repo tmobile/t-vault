fe382382a0c898d1497f6d8c1067843ae81eabd1
Sarath Mohan <sarath.mohan11@t-mobile.com>
2020-08-19 14:26:30 +0530

    Updated ldap.userdn AD configuration

--- tvaultapi/src/main/java/com/tmobile/cso/vault/api/config/DirectoryConfiguration.java
+++ tvaultapi/src/main/java/com/tmobile/cso/vault/api/config/DirectoryConfiguration.java
@@ -48,7 +48,7 @@ public class DirectoryConfiguration {
         String masterAccDN = env.getRequiredProperty("ldap.username")+","+env.getRequiredProperty("ldap.masteraccdn");
         contextSource.setUrl(env.getRequiredProperty("ldap.url"));
         contextSource.setBase(env.getRequiredProperty("ldap.base"));
-        contextSource.setUserDn(masterAccDN);
+        contextSource.setUserDn(env.getRequiredProperty("ldap.username")+","+env.getRequiredProperty("ldap.userdn"));
         contextSource.setPassword(new String(Base64.getDecoder().decode(env.getRequiredProperty("ldap.password"))));
 
         return contextSource;
