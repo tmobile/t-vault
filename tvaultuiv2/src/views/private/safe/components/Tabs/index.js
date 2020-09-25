39ce93a15ed051396a66ff88ff127205353eaa93
swatish28 <swati@piktorlabs.com>
2020-09-25 13:37:02 +0530

    disable add folder

--- tvaultuiv2/src/views/private/safe/components/Tabs/index.js
+++ tvaultuiv2/src/views/private/safe/components/Tabs/index.js
@@ -220,6 +220,7 @@ export default function SelectionTabs(props) {
               onClick={addSecretsFolder}
               customStyle={customBtnStyles}
               iconSrc={
+                safeDetail?.access?.toLowerCase() === 'read' ||
                 Object.keys(safeDetail).length === 0
                   ? disableAddFolder
                   : addFolderPlus
