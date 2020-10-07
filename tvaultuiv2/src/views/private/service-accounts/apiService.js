173e703a8d6036f46329f6645768d21dc388aa09
swati-sh <swati@piktorlabs.com>
2020-10-07 10:41:10 +0530

    rotate secrets

--- tvaultuiv2/src/views/private/service-accounts/apiService.js
+++ tvaultuiv2/src/views/private/service-accounts/apiService.js
@@ -6,8 +6,12 @@ const getServiceAccounts = () => api.get('/serviceaccounts');
 const getServiceAccountPassword = (name) =>
   api.get(`/serviceaccounts/password?serviceAccountName=${name}`);
 
+const resetServiceAccountPassword = (name, payload) =>
+  api.put(`/serviceaccounts/password?serviceAccountName=${name}`, payload);
+
 export default {
   getServiceAccounts,
   getServiceAccountList,
   getServiceAccountPassword,
+  resetServiceAccountPassword,
 };
--- tvaultuiv2/src/views/private/service-accounts/components/ServiceAccountSecrets/index.js
+++ tvaultuiv2/src/views/private/service-accounts/components/ServiceAccountSecrets/index.js
@@ -1,8 +1,10 @@
+/* eslint-disable react/jsx-wrap-multilines */
 import React, { useState, useEffect } from 'react';
 import styled, { css } from 'styled-components';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import PropTypes from 'prop-types';
 import VisibilityIcon from '@material-ui/icons/Visibility';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import IconRefreshCC from '../../../../../assets/refresh-ccw.svg';
@@ -10,6 +12,9 @@ import LoaderSpinner from '../../../../../components/Loaders/LoaderSpinner';
 import ComponentError from '../../../../../errorBoundaries/ComponentError/component-error';
 import apiService from '../../apiService';
 import lock from '../../../../../assets/icon_lock.svg';
+import ButtonComponent from '../../../../../components/FormFields/ActionButton';
+import mediaBreakpoints from '../../../../../breakpoints';
+import ConfirmationModal from '../../../../../components/ConfirmationModal';
 import {
   PopperItem,
   BackgroundColor,
@@ -71,7 +76,11 @@ const ServiceAccountSecrets = (props) => {
   const [showSecret, setShowSecret] = useState(false);
   const [responseType, setResponseType] = useState(null);
   const [toastMessage, setToastMessage] = useState('');
-
+  const [openConfirmationModal, setOpenConfirmationModal] = useState(false);
+  const isMobileScreen = useMediaQuery(mediaBreakpoints.small);
+  const handleClose = () => {
+    setOpenConfirmationModal(false);
+  };
   useEffect(() => {
     if (accountDetail && Object.keys(accountDetail).length > 0) {
       apiService
@@ -95,6 +104,27 @@ const ServiceAccountSecrets = (props) => {
     setToastMessage('Secret copied to clipboard');
   };
 
+  const onResetConfirmedClicked = () => {
+    const payload = {};
+    setOpenConfirmationModal(false);
+    setResponse({ status: 'loading' });
+    apiService
+      .resetServiceAccountPassword(accountDetail?.name, payload)
+      .then((res) => {
+        setResponse({ status: 'success' });
+        if (res?.data) {
+          setSecretsData(res.data);
+          setResponseType(1);
+          setToastMessage('Password reset successfully');
+        }
+      })
+      .catch((e) => console.log('e', e));
+  };
+
+  const onResetClicked = () => {
+    setOpenConfirmationModal(true);
+  };
+
   const onToastClose = (reason) => {
     if (reason === 'clickaway') {
       return;
@@ -105,6 +135,27 @@ const ServiceAccountSecrets = (props) => {
   return (
     <ComponentError>
       <>
+        <ConfirmationModal
+          open={openConfirmationModal}
+          handleClose={handleClose}
+          title="Are you sure you want to reset the password for this Service Account?"
+          cancelButton={
+            <ButtonComponent
+              label="Cancel"
+              color="primary"
+              onClick={() => handleClose()}
+              width={isMobileScreen ? '100%' : '38%'}
+            />
+          }
+          confirmButton={
+            <ButtonComponent
+              label="Confirm"
+              color="secondary"
+              onClick={() => onResetConfirmedClicked()}
+              width={isMobileScreen ? '100%' : '38%'}
+            />
+          }
+        />
         {response.status === 'loading' && (
           <LoaderSpinner customStyle={customStyle} />
         )}
@@ -132,7 +183,7 @@ const ServiceAccountSecrets = (props) => {
                   <span>{showSecret ? 'Hide Secret' : 'View Secret'}</span>
                 </PopperItem>
                 {accountDetail.access === 'write' && (
-                  <PopperItem>
+                  <PopperItem onClick={() => onResetClicked()}>
                     <img alt="refersh-ic" src={IconRefreshCC} />
                     <span>Rotate Secret</span>
                   </PopperItem>
@@ -153,6 +204,15 @@ const ServiceAccountSecrets = (props) => {
         {responseType === 1 && (
           <SnackbarComponent
             open
+            onClose={() => onToastClose()}
+            message={toastMessage}
+          />
+        )}
+        {responseType === -1 && (
+          <SnackbarComponent
+            open
+            severity="error"
+            icon="error"
             onClose={() => onToastClose()}
             message={toastMessage}
           />
