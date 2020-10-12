68d6ca9b9706989e39bbfa388ab5f2ac79dccc67
swati-sh <swati@piktorlabs.com>
2020-10-12 09:36:47 +0530

    fixes

--- tvaultuiv2/src/styles/GlobalStyles/index.js
+++ tvaultuiv2/src/styles/GlobalStyles/index.js
@@ -69,7 +69,7 @@ export const PopperItem = styled.div`
 `;
 
 export const TabWrapper = styled.div`
-  height: calc(100% - 2.8rem);
+  height: 100%;
   display: flex;
   flex-direction: column;
   position: relative;
