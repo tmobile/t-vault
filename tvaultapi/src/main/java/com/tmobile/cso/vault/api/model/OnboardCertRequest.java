bb65c91e098742dda840cec7cb98a6ca0e17563d
nlokana1 <nithya.lokanathan1@t-mobile.com>
2020-10-21 12:23:21 +0530

    Added objects for onboard

---
+++ tvaultapi/src/main/java/com/tmobile/cso/vault/api/model/OnboardCertRequest.java
@@ -0,0 +1,17 @@
+package com.tmobile.cso.vault.api.model;
+
+import java.io.Serializable;
+
+public class OnboardCertRequest implements Serializable {
+
+	private static final long serialVersionUID = 8693508595218686587L;
+	private String certType;
+	private String commonname;
+	private String containerName;
+	private String appOu;
+	private String containerPath;
+	
+	
+
+
+}
