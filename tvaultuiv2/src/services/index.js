75ee5ed2658159f9e9a5431aed2171fd959ba67c
swati-sh <swati@piktorlabs.com>
2020-11-25 17:29:48 +0530

    Redirect url fixes in another tab

--- tvaultuiv2/src/services/index.js
+++ tvaultuiv2/src/services/index.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import configUrl from '../config';
 
 function ApiCall(url, method, data, header) {
-  if (!sessionStorage.getItem('token')) {
+  if (!localStorage.getItem('token')) {
     window.location.href = '/';
   }
-  const token = sessionStorage.getItem('token');
+  const token = localStorage.getItem('token');
   const headers = {
     ...header,
     'vault-token': token,
