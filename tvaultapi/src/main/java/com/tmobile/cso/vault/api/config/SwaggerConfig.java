c8988590304a889265ffde0cbbecd51e543bd0d4
Nithin Nazeer(UST <144117@ust-global.com>
2020-12-17 17:02:10 +0530

    COCICCS-5549- Swagger Documentation changes

--- tvaultapi/src/main/java/com/tmobile/cso/vault/api/config/SwaggerConfig.java
+++ tvaultapi/src/main/java/com/tmobile/cso/vault/api/config/SwaggerConfig.java
@@ -31,6 +31,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -46,6 +47,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 @EnableAutoConfiguration
+@PropertySource("classpath:swaggernotes.properties")
 @ComponentScan(basePackages="com.tmobile.cso.vault.api.*")
 public class SwaggerConfig {
 
