16c950c7d8b2a3ff9e844d20f6249c0add36d9eb
swati-sh <swati@piktorlabs.com>
2021-01-07 17:26:21 +0530

    logout

--- tvaultuiv2/src/components/Header/userLogout.js
+++ tvaultuiv2/src/components/Header/userLogout.js
@@ -1,18 +1,17 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React, { useEffect, useState } from 'react';
 import * as msal from '@azure/msal-browser';
-import { makeStyles } from '@material-ui/core/styles';
-import Popper from '@material-ui/core/Popper';
-import Fade from '@material-ui/core/Fade';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom';
 import userIcon from '../../assets/icon-profile.svg';
 import vectorIcon from '../../assets/vector.svg';
 import { revokeToken } from '../../views/public/HomePage/utils';
 import configData from '../../config/config';
 import mediaBreakpoints from '../../breakpoints';
 import configUrl from '../../config/index';
+
+const LogoutWrapper = styled.div`
+  position: relative;
+`;
 
 const UserWrap = styled.div`
   display: flex;
@@ -39,34 +38,31 @@ const VectorIcon = styled.img`
   margin-left: 0.4rem;
 `;
 
-const Logout = styled.div`
-  cursor: pointer;
-`;
-
 const AdminLabel = styled.div`
   display: flex;
   font-weight: bold;
 `;
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    zIndex: '2000',
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(1),
-  },
-}));
+const LogoutPaper = styled.div`
+  display: ${(props) => (props.logoutClicked ? 'block' : 'none')};
+  position: absolute;
+  background-color: ${(props) => props.theme.palette.background.paper};
+  padding: 1rem 2rem;
+  right: 0;
+  cursor: pointer;
+  ${mediaBreakpoints.smallAndMedium} {
+    right: 50%;
+  }
+`;
 
 const UserLogout = (props) => {
   const { userName, checkToken } = props;
-  const classes = useStyles();
-  const [isAdmin, setIsAdmin] = useState(false);
-  const [anchorEl, setAnchorEl] = useState(null);
-  const history = useHistory();
 
-  const handleClick = (event) => {
-    setAnchorEl(anchorEl ? null : event.currentTarget);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [logoutClicked, setLogoutClicked] = useState(false);
+
+  const handleClick = () => {
+    setLogoutClicked(!logoutClicked);
   };
 
   const config = {
@@ -79,26 +75,25 @@ const UserLogout = (props) => {
 
   const myMsal = new msal.PublicClientApplication(config);
   const onLogoutClicked = async () => {
+    setLogoutClicked(false);
+    myMsal.logout();
     if (configData.AUTH_TYPE === 'oidc') {
       await revokeToken();
     }
     localStorage.clear();
     checkToken();
-    history.push('/');
-    myMsal.logout();
   };
 
-  const open = Boolean(anchorEl);
-  const id = open ? 'transitions-popper' : undefined;
   useEffect(() => {
     const admin = localStorage.getItem('isAdmin');
     if (admin) {
       setIsAdmin(JSON.parse(admin));
     }
   }, []);
+
   return (
-    <>
-      <UserWrap aria-describedby={id} onClick={handleClick}>
+    <LogoutWrapper>
+      <UserWrap onClick={() => handleClick()}>
         <UserIcon src={userIcon} alt="usericon" />
         <AdminLabel>
           {isAdmin && <>(Admin) </>}
@@ -106,22 +101,14 @@ const UserLogout = (props) => {
         </AdminLabel>
         <VectorIcon src={vectorIcon} alt="vectoricon" />
       </UserWrap>
-      <Popper
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        transition
-        className={classes.root}
+
+      <LogoutPaper
+        logoutClicked={logoutClicked}
+        onClick={() => onLogoutClicked()}
       >
-        {({ TransitionProps }) => (
-          <Fade {...TransitionProps} timeout={350}>
-            <Logout className={classes.paper} onClick={() => onLogoutClicked()}>
-              Logout
-            </Logout>
-          </Fade>
-        )}
-      </Popper>
-    </>
+        Logout
+      </LogoutPaper>
+    </LogoutWrapper>
   );
 };
 
