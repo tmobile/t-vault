62e1077f91f64d3976c6c22bd9f5ba2b35903c25
Sarath Mohan <sarath.mohan11@t-mobile.com>
2021-01-14 14:49:14 +0530

    Using last modified message to use moment()

--- tvaultui/src/app/Common/Directives/folderContentsRow/folderContentsRow.directive.js
+++ tvaultui/src/app/Common/Directives/folderContentsRow/folderContentsRow.directive.js
@@ -52,7 +52,7 @@
       }
     });
 
-  function folderContentsTableController($scope, CopyToClipboard, SafesManagement, Modal, UtilityService, Notifications, $rootScope, toastr, safesService, $timeout) {
+  function folderContentsTableController($scope, CopyToClipboard, SafesManagement, Modal, UtilityService, Notifications, $rootScope, toastr, safesService, $timeout, $state) {
     var vm = this;
     vm.anyRegex = /.|\s/g;
     vm.originalId = '';
@@ -90,36 +90,10 @@
     }
 
     function formatTime(timestamp) {
-      if (isNaN(timestamp)) {
-        timestamp = new Date().getTime();
+      if (!isNaN(timestamp)) {
+        return moment(timestamp).fromNow();
       }
-
-      var current = new Date();
-      var msPerMinute = 60 * 1000;
-      var msPerHour = msPerMinute * 60;
-      var msPerDay = msPerHour * 24;
-      var msPerMonth = msPerDay * 30;
-      var msPerYear = msPerDay * 365;
-
-      var elapsed = current.getTime() - timestamp;
-      if (elapsed < msPerMinute) {
-           return Math.round(elapsed/1000) + ' seconds ago';
-      }
-      else if (elapsed < msPerHour) {
-           return Math.round(elapsed/msPerMinute) + ' minutes ago';
-      }
-      else if (elapsed < msPerDay ) {
-           return Math.round(elapsed/msPerHour ) + ' hours ago';
-      }
-      else if (elapsed < msPerMonth) {
-          return Math.round(elapsed/msPerDay) + ' days ago';
-      }
-      else if (elapsed < msPerYear) {
-          return Math.round(elapsed/msPerMonth) + ' months ago';
-      }
-      else {
-          return Math.round(elapsed/msPerYear ) + ' years ago';
-      }
+      return "";
     }
 
     function edit() {
@@ -229,6 +203,7 @@
                   vm.loading(false);
                   vm.editing = false;
                   Notifications.toast('Saved successfully');
+                  $state.reload();
                 }).catch(catchError);
             })
         .catch(function () {
