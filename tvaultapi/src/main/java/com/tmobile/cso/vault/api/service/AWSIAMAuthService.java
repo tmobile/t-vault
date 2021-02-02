20adc18537674f2b3f54569d07173b03bf4ef68e
msivatmob <Midhuna.Sivadas1@T-Mobile.com>
2021-02-02 09:46:11 +0530

    error message changed.

--- tvaultapi/src/main/java/com/tmobile/cso/vault/api/service/AWSIAMAuthService.java
+++ tvaultapi/src/main/java/com/tmobile/cso/vault/api/service/AWSIAMAuthService.java
@@ -81,7 +81,7 @@ public class AWSIAMAuthService {
 					put(LogMessage.MESSAGE, String.format("Trying to create AWS IAM Role [%s]", awsiamRole.getRole())).
 					put(LogMessage.APIURL, ThreadLocalContext.getCurrentMap().get(LogMessage.APIURL).toString()).
 					build()));
-			throw new TVaultValidationException("Policies are not permitted during safe creation");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"errors\":[\"Policies are not permitted during role creation.\"]}");
 		}
 		if (!ControllerUtil.areAWSIAMRoleInputsValid(awsiamRole)) {
 			throw new TVaultValidationException("Invalid inputs for the given aws login type");
