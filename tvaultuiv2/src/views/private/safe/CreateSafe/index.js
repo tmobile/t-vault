3b2e6f1d9bca4c128ef77d2b11130a1ac314651b
swati-sh <swati@piktorlabs.com>
2021-02-23 18:53:34 +0530

    disabled transfer fixes

--- tvaultuiv2/src/views/private/safe/CreateSafe/index.js
+++ tvaultuiv2/src/views/private/safe/CreateSafe/index.js
@@ -566,7 +566,7 @@ const CreateModal = (props) => {
   };
 
   const onAppNameChange = (e) => {
-    if (e?.target?.value) {
+    if (e?.target?.value || e?.target?.value === '') {
       setApplicationName(e?.target?.value);
     }
   };
@@ -806,6 +806,7 @@ const CreateModal = (props) => {
                               setOpenTransferConfirmationModal(true)
                             }
                             width={isMobileScreen ? '100%' : ''}
+                            disabled={!safeDetails.appName && editSafe}
                           />
                         </CancelButton>
                       )}
