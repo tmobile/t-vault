2583eb37980444b7ffd5038b613dd1d425fdb12c
alexrajan3 <alex.anantha3@t-mobile.com>
2021-02-24 22:21:29 +0530

    Pagination changes and Onload pending certificate issue fix
    # Conflicts:
    #	tvaultapi/src/main/java/com/tmobile/cso/vault/api/service/SSLCertificateService.java

--- tvaultapi/src/main/java/com/tmobile/cso/vault/api/v2/controller/SSLCertificateController.java
+++ tvaultapi/src/main/java/com/tmobile/cso/vault/api/v2/controller/SSLCertificateController.java
@@ -51,7 +51,7 @@ public class SSLCertificateController {
 	 * @param certManagetLoginRequest
 	 * @return
 	 */
-	@ApiOperation(value = "${SSLCertificateController.login.value}", notes = "${SSLCertificateController.login.notes}", hidden = true)
+	@ApiOperation(value = "${SSLCertificateController.login.value}", notes = "${SSLCertificateController.login.notes}", hidden = false)
 	@PostMapping(value="/v2/auth/sslcert/login",produces="application/json")
 	public ResponseEntity<String> authenticate(@RequestBody CertManagerLoginRequest certManagetLoginRequest) throws Exception {
 		return sslCertificateService.authenticate(certManagetLoginRequest);
@@ -80,7 +80,7 @@ public class SSLCertificateController {
 	public ResponseEntity<String> getCertificates(HttpServletRequest request, @RequestHeader(value="vault-token") String token, @RequestParam(name="certificateName", required = false) String certName,@RequestParam(name = "limit", required = false) Integer limit,
 			@RequestParam(name = "offset", required = false) Integer offset, @RequestParam(name = "certType", required = true) String certType){
 		UserDetails userDetails = (UserDetails) request.getAttribute(USER_DETAILS_STRING);
-		return sslCertificateService.getServiceCertificates(token, userDetails, certName, limit, offset,certType);
+		return sslCertificateService.getAllSSLCertificatesToManage(token, userDetails, certName, limit, offset,certType);
      }
 	
 	/**
